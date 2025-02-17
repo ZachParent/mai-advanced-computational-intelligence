c06d822005208fa8ffc126bb930f08fc2636aa48
Zach Parent <zachparent@duck.com>
2025-02-17 18:01:31 +0100

    fill in frozen_lake_q.py from tutorial

--- frozen_lake_q.py
+++ frozen_lake_q.py
@@ -1,31 +1,69 @@
 import gymnasium as gym
 import numpy as np
-def main():
-    env = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=False, render_mode='human')
-    q_table = np.zeros((env.observation_space.n, env.action_space.n))
-    learning_rate = 0.9
-    discount_rate = 0.9
-    epsilon = 1.0
-    epsilon_min = 0.01
-    epsilon_decay_rate = 0.0001
-    rng = np.random.default_rng()
+import matplotlib.pyplot as plt
+import pickle
 
-    num_episodes = 10
-    for episode in range(num_episodes):
-        state = env.reset()[0]
-        terminated = False
-        truncated = False
+def run(episodes, is_training=True, render=False):
 
-        while not terminated and not truncated:
-            if rng.random() < epsilon:
-                action = rng.choice(env.action_space.n)
+    env = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=False, render_mode='human' if render else None)
+
+    if(is_training):
+        q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
+    else:
+        f = open('frozen_lake8x8.pkl', 'rb')
+        q = pickle.load(f)
+        f.close()
+
+    learning_rate_a = 0.9 # alpha or learning rate
+    discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
+    epsilon = 1         # 1 = 100% random actions
+    epsilon_decay_rate = 0.0001        # epsilon decay rate. 1/0.0001 = 10,000
+    rng = np.random.default_rng()   # random number generator
+
+    rewards_per_episode = np.zeros(episodes)
+
+    for i in range(episodes):
+        state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
+        terminated = False      # True when fall in hole or reached goal
+        truncated = False       # True when actions > 200
+
+        while(not terminated and not truncated):
+            if is_training and rng.random() < epsilon:
+                action = env.action_space.sample() # actions: 0=left,1=down,2=right,3=up
             else:
-                action = np.argmax(q_table[state])
-            next_state, reward, terminated, truncated, info = env.step(action)
-            q_table[state, action] = q_table[state, action] + learning_rate * (reward + discount_rate * np.max(q_table[next_state]) - q_table[state, action])
-            state = next_state
-            epsilon = max(epsilon - epsilon_decay_rate, epsilon_min)
+                action = np.argmax(q[state,:])
+
+            new_state,reward,terminated,truncated,_ = env.step(action)
+
+            if is_training:
+                q[state,action] = q[state,action] + learning_rate_a * (
+                    reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action]
+                )
+
+            state = new_state
+
+        epsilon = max(epsilon - epsilon_decay_rate, 0)
+
+        if(epsilon==0):
+            learning_rate_a = 0.0001
+
+        if reward == 1:
+            rewards_per_episode[i] = 1
+
     env.close()
 
-if __name__ == "__main__":
-    main()
+    sum_rewards = np.zeros(episodes)
+    for t in range(episodes):
+        sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
+    plt.plot(sum_rewards)
+    plt.savefig('frozen_lake8x8.png')
+
+    if is_training:
+        f = open("frozen_lake8x8.pkl","wb")
+        pickle.dump(q, f)
+        f.close()
+
+if __name__ == '__main__':
+    run(15000)
+
+    # run(1, is_training=False, render=True)

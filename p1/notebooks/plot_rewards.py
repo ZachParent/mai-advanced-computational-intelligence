463de508f6f952a0b67969a37552de5ebbe11187
Zach Parent <zachparent@duck.com>
2025-04-25 17:33:49 +0200

    plot p values in heatmap

--- p1/notebooks/plot_rewards.py
+++ p1/notebooks/plot_rewards.py
@@ -3,7 +3,11 @@ import sys
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+from einops import rearrange
 
 PROJECT_ROOT = Path(__file__).parent.parent
 sys.path.append(str(PROJECT_ROOT))
@@ -12,6 +16,7 @@ from src.config import RESULTS_DIR
 from src.run_config import PENDULUM_CONFIGS
 
 plt.style.use("default")
+WINDOW_SIZE = 20
 
 # %%
 # plot reward over episodes
@@ -22,16 +27,14 @@ for i, run_config in enumerate(PENDULUM_CONFIGS):
     experiment_id = run_config.id
     try:
         results = pd.read_csv(RESULTS_DIR / f"{experiment_id:03d}.csv")
-        results.columns = ["episode", "reward", "length"]
     except FileNotFoundError:
         print(f"Experiment {experiment_id} not found")
         continue
 
     ax = axs[i % n_rows, i // n_rows]
-    window_size = 20  # You can adjust the window size for the moving average
-    moving_average = results["reward"].rolling(window=window_size).mean()
+    moving_average = results["rewards"].rolling(window=WINDOW_SIZE).mean()
 
-    ax.plot(results["reward"], alpha=0.4, label="Original Rewards", color="dodgerblue")
+    ax.plot(results["rewards"], alpha=0.4, label="Original Rewards", color="dodgerblue")
     ax.plot(moving_average, label="Moving Average", color="blueviolet")
     ax.set_xlabel("Episode")
     ax.set_ylabel("Reward")
@@ -45,6 +48,44 @@ for i, run_config in enumerate(PENDULUM_CONFIGS):
         ha="center",
         va="center",
     )
+plt.tight_layout()
+plt.show()
+# %%
+hist_dfs = {}
+for run_config in PENDULUM_CONFIGS:
+    experiment_id = run_config.id
+    try:
+        hist_dfs[experiment_id] = pd.read_csv(RESULTS_DIR / f"{experiment_id:03d}.csv")
+    except FileNotFoundError:
+        print(f"Experiment {experiment_id} not found")
+        continue
+# %%
+
+sum_of_rewards = np.array([df["rewards"].sum() for df in hist_dfs.values()])
+sum_of_final_rewards = np.array(
+    [df["rewards"].iloc[-WINDOW_SIZE:].sum() for df in hist_dfs.values()]
+)
+# TODO: remove this truncation once all data is collected
+sum_of_final_rewards = sum_of_final_rewards[: (len(sum_of_final_rewards) // 5) * 5]
+sum_of_final_rewards = rearrange(
+    sum_of_final_rewards, "(config seed) -> config seed", seed=5
+)
+# %%
+pval_grid = np.ones((len(PENDULUM_CONFIGS) // 5, len(PENDULUM_CONFIGS) // 5))
+for i in range(len(sum_of_final_rewards)):
+    for j in range(len(sum_of_final_rewards)):
+        if i == j:
+            pval_grid[i, j] = 1
+        else:
+            ttest, pval = stats.ttest_rel(
+                sum_of_final_rewards[i], sum_of_final_rewards[j]
+            )
+            pval_grid[i, j] = pval
+fig, ax = plt.subplots(figsize=(12, 10))
+sns.heatmap(pval_grid, annot=True, cmap="PuBuGn_r", vmin=0, vmax=1, ax=ax)
+ax.set_title(f"P-values of Rewards from Final {WINDOW_SIZE} Episodes")
+ax.set_xlabel("Experiment")
+ax.set_ylabel("Experiment")
 plt.tight_layout()
 plt.show()
 # %%

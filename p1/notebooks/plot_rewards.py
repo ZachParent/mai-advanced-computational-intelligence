c3cae98b1f53fd1ab7383cc74aeb14b8c9bb4182
Zach Parent <zachparent@duck.com>
2025-04-25 17:03:38 +0200

    plot rewards for all configs

--- p1/notebooks/plot_rewards.py
+++ p1/notebooks/plot_rewards.py
@@ -9,18 +9,42 @@ PROJECT_ROOT = Path(__file__).parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
 from src.config import RESULTS_DIR
+from src.run_config import PENDULUM_CONFIGS
+
+plt.style.use("default")
 
 # %%
-experiment_id = 4
+# plot reward over episodes
+n_rows = 5
+n_cols = len(PENDULUM_CONFIGS) // n_rows
+fig, axs = plt.subplots(n_rows, n_cols, figsize=(3 * n_cols, 3 * n_rows))
+for i, run_config in enumerate(PENDULUM_CONFIGS):
+    experiment_id = run_config.id
+    try:
+        results = pd.read_csv(RESULTS_DIR / f"{experiment_id:03d}.csv")
+        results.columns = ["episode", "reward", "length"]
+    except FileNotFoundError:
+        print(f"Experiment {experiment_id} not found")
+        continue
 
-results = pd.read_csv(RESULTS_DIR / f"{experiment_id:03d}.csv")
-results.columns = ["episode", "reward", "length"]
+    ax = axs[i % n_rows, i // n_rows]
+    window_size = 20  # You can adjust the window size for the moving average
+    moving_average = results["reward"].rolling(window=window_size).mean()
 
-window_size = 10  # You can adjust the window size for the moving average
-moving_average = results["reward"].rolling(window=window_size).mean()
-plt.plot(moving_average, label="Moving Average", color="orange")
-plt.plot(results["reward"], alpha=0.5, label="Original Rewards", color="blue")
-plt.legend()
+    ax.plot(results["reward"], alpha=0.4, label="Original Rewards", color="dodgerblue")
+    ax.plot(moving_average, label="Moving Average", color="blueviolet")
+    ax.set_xlabel("Episode")
+    ax.set_ylabel("Reward")
+    ax.set_title(f"Experiment {experiment_id}")
+    ax.legend(loc="lower right")
+    ax.text(
+        0.5,
+        0.5,
+        f"Final Reward: {moving_average.iloc[-1]:.2f}",
+        transform=ax.transAxes,
+        ha="center",
+        va="center",
+    )
+plt.tight_layout()
 plt.show()
-
 # %%
